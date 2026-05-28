471f1ecdcda5b6cf9d03d5d99cdc6d939eeccc48
SonChanWoo <son556@all-conec.com>
2026-05-29 00:08:12 +0900

    기본 기능 추가 중

--- SetOpenGL/HalfEdge.cpp
+++ SetOpenGL/HalfEdge.cpp
@@ -1,19 +1,19 @@
-﻿#include "HalfEdge.h"
+#include "HalfEdge.h"
 #include <map>
 #include <set>
 #include <iostream>
 
-bool HalfEdge::CheckValidManifold(const std::vector<size_t>& indices)
+bool HalfEdge::CheckValidManifold(const std::vector<int>& indices)
 {
     return false;
 }
 
-void HalfEdge::preProcess(triangle_t* triangles, size_t triangleSize, std::vector<edge_t>& outEdges)
+void HalfEdge::preProcess(triangle_t* triangles, int triangleSize, std::vector<edge_t>& outEdges)
 {
     outEdges.clear();
     outEdges.reserve(triangleSize * 3);
 
-    for (size_t i = 0; i < triangleSize; ++i)
+    for (int i = 0; i < triangleSize; ++i)
     {
         edge_t edge;
 
@@ -32,25 +32,30 @@ void HalfEdge::preProcess(triangle_t* triangles, size_t triangleSize, std::vecto
 }
 
 
-size_t FindFaceIdByEdge(size_t v0Id, size_t v1Id, const std::map<std::pair<size_t, size_t>, size_t>& book)
+int FindFaceIdByEdge(int v0Id, int v1Id, const std::map<std::pair<int, int>, int>& book)
 {
-    std::map<std::pair<size_t, size_t>, size_t>::const_iterator it = book.find({ v0Id, v1Id });
+    std::map<std::pair<int, int>, int>::const_iterator it = book.find({ v0Id, v1Id });
     if (it == book.end())
-        return INVALID_INDEX;
+        return -1;
     else
         return it->second;
 }
 
 void HalfEdge::Build(
-    const size_t numVertices,
-    const size_t numTriangles,
+    const int numVertices,
+    const int numTriangles,
     const triangle_t* triangles,
-    const size_t numEdges,
-    const edge_t* edges)
+    const int numEdges,
+    const edge_t* edges,
+    const std::vector<glm::vec3>& points
+)
 {
-    std::map<std::pair<size_t, size_t>, size_t> edgeAndFaceBook;
+    for (const auto& point : points)
+        _points.push_back(point);
 
-    for (size_t faceIndex = 0; faceIndex < numTriangles; ++faceIndex)
+    std::map<std::pair<int, int>, int> edgeAndFaceBook;
+
+    for (int faceIndex = 0; faceIndex < numTriangles; ++faceIndex)
     {
         const triangle_t& tri = triangles[faceIndex];
         edgeAndFaceBook[{tri.vertices[0], tri.vertices[1]}] = faceIndex;
@@ -60,20 +65,20 @@ void HalfEdge::Build(
 
     Clear();
 
-    _vertexHalfEdges.resize(numVertices, INVALID_INDEX);
-    _edgeHalfEdges.resize(numEdges, INVALID_INDEX);
-    _faceHalfEdges.resize(numTriangles, INVALID_INDEX);
+    _vertexHalfEdges.resize(numVertices, -1);
+    _edgeHalfEdges.resize(numEdges, -1);
+    _faceHalfEdges.resize(numTriangles, -1);
     _halfEdges.reserve(numEdges * 2);
 
-    for (size_t ei = 0; ei < numEdges; ++ei)
+    for (int ei = 0; ei < numEdges; ++ei)
     {
         const edge_t& edge = edges[ei];
 
-        const size_t h0Index = _halfEdges.size();
+        const int h0Index = (int)_halfEdges.size();
         _halfEdges.push_back(halfedge_t());
         halfedge_t& he0 = _halfEdges.back();
 
-        const size_t h1Index = _halfEdges.size();
+        const int h1Index = (int)_halfEdges.size();
         _halfEdges.push_back(halfedge_t());
         halfedge_t& he1 = _halfEdges.back();
 
@@ -91,35 +96,35 @@ void HalfEdge::Build(
         _edgeToHalfEdge[{edge.start, edge.end}] = h0Index;
         _edgeToHalfEdge[{edge.end, edge.start}] = h1Index;
 
-        if (_vertexHalfEdges[he0.toVertex] == INVALID_INDEX || he1.faceIndex == INVALID_INDEX)
+        if (_vertexHalfEdges[he0.toVertex] == -1 || he1.faceIndex == -1)
             _vertexHalfEdges[he0.toVertex] = he0.oppositeHalfEdge;
 
-        if (_vertexHalfEdges[he1.toVertex] == INVALID_INDEX || he0.faceIndex == INVALID_INDEX)
+        if (_vertexHalfEdges[he1.toVertex] == -1 || he0.faceIndex == -1)
             _vertexHalfEdges[he1.toVertex] = he1.oppositeHalfEdge;
 
-        if (he0.faceIndex != INVALID_INDEX && _faceHalfEdges[he0.faceIndex] == INVALID_INDEX)
+        if (he0.faceIndex != -1 && _faceHalfEdges[he0.faceIndex] == -1)
             _faceHalfEdges[he0.faceIndex] = h0Index;
 
-        if (he1.faceIndex != INVALID_INDEX && _faceHalfEdges[he1.faceIndex] == INVALID_INDEX)
+        if (he1.faceIndex != -1 && _faceHalfEdges[he1.faceIndex] == -1)
             _faceHalfEdges[he1.faceIndex] = h1Index;
 
         _edgeHalfEdges[ei] = h0Index;
     }
 
-    std::vector<size_t> boundaryHalfEdges;
-    for (size_t hei = 0; hei < _halfEdges.size(); ++hei)
+    std::vector<int> boundaryHalfEdges;
+    for (int hei = 0; hei < (int)_halfEdges.size(); ++hei)
     {
         halfedge_t& he = _halfEdges[hei];
-        if (he.faceIndex == INVALID_INDEX)
+        if (he.faceIndex == -1)
         {
             boundaryHalfEdges.push_back(hei);
             continue;
         }
 
         const triangle_t& face = triangles[he.faceIndex];
-        const size_t vi = he.toVertex;
+        const int vi = he.toVertex;
 
-        size_t vj = INVALID_INDEX;
+        int vj = -1;
         if (face.vertices[0] == vi) vj = face.vertices[1];
         else if (face.vertices[1] == vi) vj = face.vertices[2];
         else if (face.vertices[2] == vi) vj = face.vertices[0];
@@ -127,10 +132,10 @@ void HalfEdge::Build(
         he.nextHlafEdge = _edgeToHalfEdge[{vi, vj}];
     }
 
-    std::map<size_t, std::set<size_t>> vertex2OutgoingBoundaryHalfEdgeIndices;
-    for (std::vector<size_t>::const_iterator hei = boundaryHalfEdges.begin(); hei != boundaryHalfEdges.end(); ++hei)
+    std::map<int, std::set<int>> vertex2OutgoingBoundaryHalfEdgeIndices;
+    for (std::vector<int>::const_iterator hei = boundaryHalfEdges.begin(); hei != boundaryHalfEdges.end(); ++hei)
     {
-        const size_t originateVertexIndex = _halfEdges[_halfEdges[*hei].oppositeHalfEdge].toVertex;
+        const int originateVertexIndex = _halfEdges[_halfEdges[*hei].oppositeHalfEdge].toVertex;
         vertex2OutgoingBoundaryHalfEdgeIndices[originateVertexIndex].insert(*hei);
         if (vertex2OutgoingBoundaryHalfEdgeIndices[originateVertexIndex].size() > 1)
         {
@@ -138,30 +143,30 @@ void HalfEdge::Build(
         }
     }
 
-    for (std::vector<size_t>::const_iterator hei = boundaryHalfEdges.begin(); hei != boundaryHalfEdges.end(); ++hei)
+    for (std::vector<int>::const_iterator hei = boundaryHalfEdges.begin(); hei != boundaryHalfEdges.end(); ++hei)
     {
         halfedge_t& he = _halfEdges[*hei];
 
-        std::set<size_t>& outgoing = vertex2OutgoingBoundaryHalfEdgeIndices[he.toVertex];
+        std::set<int>& outgoing = vertex2OutgoingBoundaryHalfEdgeIndices[he.toVertex];
         if (outgoing.empty() == false)
         {
-            std::set<size_t>::iterator outgoingIter = outgoing.begin();
+            std::set<int>::iterator outgoingIter = outgoing.begin();
             he.nextHlafEdge = *outgoingIter;
             outgoing.erase(outgoingIter);
         }
     }
 }
 
-void HalfEdge::GetBoundaryVertices(size_t boundaryVertexIndex, std::vector<size_t>& outBoundaryVertexIndices) const
+void HalfEdge::GetBoundaryVertices(int boundaryVertexIndex, std::vector<int>& outBoundaryVertexIndices) const
 {
-    size_t hei = _vertexHalfEdges[boundaryVertexIndex];
+    int hei = _vertexHalfEdges[boundaryVertexIndex];
     if (IsBoundaryVertex(boundaryVertexIndex) == false)
         return;
 
-    if (_halfEdges[hei].faceIndex != INVALID_INDEX)
+    if (_halfEdges[hei].faceIndex != -1)
         hei = _halfEdges[hei].oppositeHalfEdge;
 
-    size_t start = hei;
+    int start = hei;
     outBoundaryVertexIndices.push_back(boundaryVertexIndex);
 
     hei = _halfEdges[hei].nextHlafEdge;
@@ -173,10 +178,10 @@ void HalfEdge::GetBoundaryVertices(size_t boundaryVertexIndex, std::vector<size_
     }
 }
 
-void HalfEdge::GetVertexIndicesOneRing(size_t vertexIndex, std::vector<size_t>& outRingVertexIndices) const
+void HalfEdge::GetVertexIndicesOneRing(int vertexIndex, std::vector<int>& outRingVertexIndices) const
 {
-    size_t hei = _vertexHalfEdges[vertexIndex];
-    size_t endVertex = _halfEdges[hei].toVertex;
+    int hei = _vertexHalfEdges[vertexIndex];
+    int endVertex = _halfEdges[hei].toVertex;
     outRingVertexIndices.push_back(endVertex);
     hei = _halfEdges[_halfEdges[hei].oppositeHalfEdge].nextHlafEdge;
 
@@ -187,14 +192,14 @@ void HalfEdge::GetVertexIndicesOneRing(size_t vertexIndex, std::vector<size_t>& 
     }
 }
 
-void HalfEdge::GetFaceVertices(size_t faceIndex, std::vector<size_t>& outFaceVertexIndices) const
+void HalfEdge::GetFaceVertices(int faceIndex, std::vector<int>& outFaceVertexIndices) const
 {
-    if (faceIndex == INVALID_INDEX)
+    if (faceIndex == -1)
         return;
 
-    size_t hei = _faceHalfEdges[faceIndex];
-    size_t endVertexIndex = _halfEdges[_halfEdges[hei].oppositeHalfEdge].toVertex;
-    size_t vertexIndex = _halfEdges[hei].toVertex;
+    int hei = _faceHalfEdges[faceIndex];
+    int endVertexIndex = _halfEdges[_halfEdges[hei].oppositeHalfEdge].toVertex;
+    int vertexIndex = _halfEdges[hei].toVertex;
 
     outFaceVertexIndices.push_back(endVertexIndex);
     while (vertexIndex != endVertexIndex)
@@ -205,48 +210,48 @@ void HalfEdge::GetFaceVertices(size_t faceIndex, std::vector<size_t>& outFaceVer
     }
 }
 
-void HalfEdge::GetFaceIndicesOneRing(size_t vertexIndex, std::vector<size_t>& outFaceIndices) const
+void HalfEdge::GetFaceIndicesOneRing(int vertexIndex, std::vector<int>& outFaceIndices) const
 {
-    size_t hei = _vertexHalfEdges[vertexIndex];
-    size_t end = _halfEdges[hei].toVertex;
+    int hei = _vertexHalfEdges[vertexIndex];
+    int end = _halfEdges[hei].toVertex;
 
-    if (_halfEdges[hei].faceIndex != INVALID_INDEX)
+    if (_halfEdges[hei].faceIndex != -1)
         outFaceIndices.push_back(_halfEdges[hei].faceIndex);
 
     hei = _halfEdges[_halfEdges[hei].oppositeHalfEdge].nextHlafEdge;
     while (_halfEdges[hei].toVertex != end)
     {
-        if (_halfEdges[hei].faceIndex != INVALID_INDEX)
+        if (_halfEdges[hei].faceIndex != -1)
             outFaceIndices.push_back(_halfEdges[hei].faceIndex);
         hei = _halfEdges[_halfEdges[hei].oppositeHalfEdge].nextHlafEdge;
     }
 }
 
-void HalfEdge::GetAdjFaceIndices(size_t faceIndex, std::vector<size_t>& outFaceIndices) const
+void HalfEdge::GetAdjFaceIndices(int faceIndex, std::vector<int>& outFaceIndices) const
 {
-    if (faceIndex == INVALID_INDEX) return;
+    if (faceIndex == -1) return;
 
-    size_t hei = _faceHalfEdges[faceIndex];
-    size_t end = _halfEdges[_halfEdges[hei].oppositeHalfEdge].toVertex;
+    int hei = _faceHalfEdges[faceIndex];
+    int end = _halfEdges[_halfEdges[hei].oppositeHalfEdge].toVertex;
 
     do
     {
-        size_t fidx = _halfEdges[_halfEdges[hei].oppositeHalfEdge].faceIndex;
-        if (fidx != INVALID_INDEX)
+        int fidx = _halfEdges[_halfEdges[hei].oppositeHalfEdge].faceIndex;
+        if (fidx != -1)
             outFaceIndices.push_back(fidx);
         hei = _halfEdges[hei].nextHlafEdge;
     } while (_halfEdges[_halfEdges[hei].oppositeHalfEdge].toVertex != end);
 }
 
 
-glm::vec3 HalfEdge::GetVertexNormal(size_t vertexIndex) const
+glm::vec3 HalfEdge::GetVertexNormal(int vertexIndex) const
 {
-    std::vector<size_t> faceIndices;
+    std::vector<int> faceIndices;
     glm::vec3 normal = glm::vec3(0, 0, 0);
 
     GetFaceIndicesOneRing(vertexIndex, faceIndices);
 
-    for (size_t i = 0; i < faceIndices.size(); ++i)
+    for (int i = 0; i < (int)faceIndices.size(); ++i)
     {
         normal += GetFaceNormalNotNormalized(faceIndices[i]);
     }
@@ -254,11 +259,11 @@ glm::vec3 HalfEdge::GetVertexNormal(size_t vertexIndex) const
     return glm::normalize(normal);
 }
 
-glm::vec3 HalfEdge::GetFaceNormal(size_t faceIndex) const
+glm::vec3 HalfEdge::GetFaceNormal(int faceIndex) const
 {
-    if (faceIndex == INVALID_INDEX) return glm::vec3(0);
+    if (faceIndex == -1) return glm::vec3(0);
 
-    std::vector<size_t> vertices;
+    std::vector<int> vertices;
     GetFaceVertices(faceIndex, vertices);
 
     glm::vec3 e0 = _points[vertices[1]] - _points[vertices[0]];
@@ -268,7 +273,7 @@ glm::vec3 HalfEdge::GetFaceNormal(size_t faceIndex) const
 }
 
 
-void HalfEdge::SplitEdge(size_t edgeIdx, float t)
+void HalfEdge::SplitEdge(int edgeIdx, float t)
 {
     _halfEdges.reserve(_halfEdges.size() + 6);
     _edgeHalfEdges.reserve(_edgeHalfEdges.size() + 3);
@@ -277,12 +282,12 @@ void HalfEdge::SplitEdge(size_t edgeIdx, float t)
     halfedge_t& halfEdgeBtoC = _halfEdges[_edgeHalfEdges[edgeIdx]];
     halfedge_t& halfEdgeCtoB = _halfEdges[halfEdgeBtoC.oppositeHalfEdge];
 
-    size_t nextCtoBHalfEdgeIdx = halfEdgeCtoB.nextHlafEdge;
+    int nextCtoBHalfEdgeIdx = halfEdgeCtoB.nextHlafEdge;
 
-    size_t halfEdgesSize = _halfEdges.size();
-    size_t edgeHalfEdgesSize = _edgeHalfEdges.size();
-    size_t faceHalfEdgesSize = _faceHalfEdges.size();
-    size_t vertexHalfEdgesSize = _vertexHalfEdges.size();
+    int halfEdgesSize = (int)_halfEdges.size();
+    int edgeHalfEdgesSize = (int)_edgeHalfEdges.size();
+    int faceHalfEdgesSize = (int)_faceHalfEdges.size();
+    int vertexHalfEdgesSize = (int)_vertexHalfEdges.size();
 
     halfedge_t* halfEdgeAtoB = nullptr;
     halfedge_t* halfEdgeCtoA = nullptr;
@@ -299,12 +304,12 @@ void HalfEdge::SplitEdge(size_t edgeIdx, float t)
     _vertexHalfEdges.push_back(_edgeHalfEdges[edgeIdx]);
 
     bool aFlag = false;
-    if (halfEdgeBtoC.faceIndex != INVALID_INDEX)
+    if (halfEdgeBtoC.faceIndex != -1)
     {
         aFlag = true;
         halfEdgeCtoA = &(_halfEdges[halfEdgeBtoC.nextHlafEdge]);
         halfEdgeAtoB = &(_halfEdges[halfEdgeCtoA->nextHlafEdge]);
-        
+
         // face
         _faceHalfEdges[halfEdgeBtoC.faceIndex] = halfEdgeBtoC.nextHlafEdge;
         _faceHalfEdges.push_back(halfEdgeCtoA->nextHlafEdge);
@@ -362,7 +367,7 @@ void HalfEdge::SplitEdge(size_t edgeIdx, float t)
 
         // half edge
         newHalfEdgeBtoM.edgeIndex = edgeIdx;
-        newHalfEdgeBtoM.faceIndex = INVALID_INDEX;
+        newHalfEdgeBtoM.faceIndex = -1;
         newHalfEdgeBtoM.oppositeHalfEdge = halfEdgesSize + 1;
         newHalfEdgeBtoM.nextHlafEdge = halfEdgeCtoB.oppositeHalfEdge;
         newHalfEdgeBtoM.toVertex = vertexHalfEdgesSize;
@@ -383,13 +388,13 @@ void HalfEdge::SplitEdge(size_t edgeIdx, float t)
         _halfEdges.push_back(newHalfEdgeMtoB);
     }
 
-    edgeHalfEdgesSize = _edgeHalfEdges.size();
-    faceHalfEdgesSize = _faceHalfEdges.size();
-    halfEdgesSize = _halfEdges.size();
+    edgeHalfEdgesSize = (int)_edgeHalfEdges.size();
+    faceHalfEdgesSize = (int)_faceHalfEdges.size();
+    halfEdgesSize = (int)_halfEdges.size();
 
-    if (halfEdgeCtoB.faceIndex != INVALID_INDEX)
+    if (halfEdgeCtoB.faceIndex != -1)
     {
-        size_t mtobIndex = aFlag ? halfEdgesSize - 3 : halfEdgesSize - 1;
+        int mtobIndex = aFlag ? halfEdgesSize - 3 : halfEdgesSize - 1;
 
         halfedge_t* halfEdgeMtoB = &(_halfEdges[mtobIndex]);
 
@@ -406,10 +411,10 @@ void HalfEdge::SplitEdge(size_t edgeIdx, float t)
         // half edge
         halfEdgeMtoB->faceIndex = faceHalfEdgesSize;
         halfEdgeMtoB->nextHlafEdge = halfEdgeCtoB.nextHlafEdge;
-        
+
         halfEdgeBtoD.faceIndex = faceHalfEdgesSize;
         halfEdgeBtoD.nextHlafEdge = halfEdgesSize;
-        
+
         halfedge_t newHalfEdgeDtoM;
         newHalfEdgeDtoM.edgeIndex = edgeHalfEdgesSize;
         newHalfEdgeDtoM.faceIndex = faceHalfEdgesSize;
@@ -431,17 +436,17 @@ void HalfEdge::SplitEdge(size_t edgeIdx, float t)
     }
     else
     {
-        size_t mtobIndex = aFlag ? halfEdgesSize - 3 : halfEdgesSize - 1;
+        int mtobIndex = aFlag ? halfEdgesSize - 3 : halfEdgesSize - 1;
 
         halfedge_t* halfEdgeMtoB = &(_halfEdges[mtobIndex]);
 
-        halfEdgeMtoB->faceIndex = INVALID_INDEX;
+        halfEdgeMtoB->faceIndex = -1;
         halfEdgeMtoB->nextHlafEdge = nextCtoBHalfEdgeIdx;
 
-        halfEdgeCtoM.faceIndex = INVALID_INDEX;
+        halfEdgeCtoM.faceIndex = -1;
         halfEdgeCtoM.nextHlafEdge = _halfEdges[halfEdgeMtoB->oppositeHalfEdge].oppositeHalfEdge;
 
-        halfEdgeMtoB->faceIndex = INVALID_INDEX;
+        halfEdgeMtoB->faceIndex = -1;
     }
 }
 
@@ -453,11 +458,11 @@ void HalfEdge::Clear()
     _halfEdges.clear();
 }
 
-glm::vec3 HalfEdge::GetFaceNormalNotNormalized(size_t faceIndex) const
+glm::vec3 HalfEdge::GetFaceNormalNotNormalized(int faceIndex) const
 {
-    if (faceIndex == INVALID_INDEX) return glm::vec3(0);
+    if (faceIndex == -1) return glm::vec3(0);
 
-    std::vector<size_t> vertices;
+    std::vector<int> vertices;
     GetFaceVertices(faceIndex, vertices);
 
     glm::vec3 e0 = _points[vertices[1]] - _points[vertices[0]];
--- SetOpenGL/HalfEdge.h
+++ SetOpenGL/HalfEdge.h
@@ -1,81 +1,98 @@
-﻿#pragma once
+#pragma once
 
 #include <vector>
 #include <map>
-#include <cstddef>
 #include <GLM/glm.hpp>
-
-constexpr size_t INVALID_INDEX = static_cast<size_t>(-1);
 
 struct edge_t
 {
-    size_t start = INVALID_INDEX;
-    size_t end = INVALID_INDEX;
+    int start = -1;
+    int end = -1;
 };
 
 struct triangle_t
 {
-    size_t vertices[3] = { INVALID_INDEX, INVALID_INDEX, INVALID_INDEX };
+    int vertices[3] = { -1, -1, -1 };
 };
 
 
 class HalfEdge
 {
 public:
-    struct halfedge_t
-    {
-        size_t toVertex = INVALID_INDEX;
-        size_t faceIndex = INVALID_INDEX;
-        size_t edgeIndex = INVALID_INDEX;
-        size_t oppositeHalfEdge = INVALID_INDEX;
-        size_t nextHlafEdge = INVALID_INDEX;
-    };
-
-    bool CheckValidManifold(const std::vector<size_t>& indices);
+    bool CheckValidManifold(const std::vector<int>& indices);
     void preProcess(
         triangle_t* triangles,
-        size_t triangleSize,
+        int triangleSize,
         std::vector<edge_t>& outEdges
     );
 
     void Build(
-        const size_t numVertices,
-        const size_t numTriangles,
+        const int numVertices,
+        const int numTriangles,
         const triangle_t* triangles,
-        const size_t numEdges,
-        const edge_t* edges
+        const int numEdges,
+        const edge_t* edges,
+        const std::vector<glm::vec3>& points
     );
 
-    void GetBoundaryVertices(size_t boundaryVertexIndex, std::vector<size_t>& outBoundaryVertexIndices) const;
-    void GetVertexIndicesOneRing(size_t vertexIndex, std::vector<size_t>& outRingVertexIndices) const;
-    void GetFaceVertices(size_t faceIndex, std::vector<size_t>& outFaceVertexIndices) const;
-    void GetFaceIndicesOneRing(size_t vertexIndex, std::vector<size_t>& outFaceIndices) const;
-    void GetAdjFaceIndices(size_t faceIndex, std::vector<size_t>& outFaceIndices) const;
-    glm::vec3 GetVertexNormal(size_t vertexIndex) const;
-    glm::vec3 GetFaceNormal(size_t faceIndex) const;
+    void SplitEdge(int edgeIdx, float t);
 
-    inline bool IsBoundaryVertex(size_t vertexIndex) const
+    void GetBoundaryVertices(int boundaryVertexIndex, std::vector<int>& outBoundaryVertexIndices) const;
+    void GetVertexIndicesOneRing(int vertexIndex, std::vector<int>& outRingVertexIndices) const;
+    void GetFaceVertices(int faceIndex, std::vector<int>& outFaceVertexIndices) const;
+    void GetFaceIndicesOneRing(int vertexIndex, std::vector<int>& outFaceIndices) const;
+    void GetAdjFaceIndices(int faceIndex, std::vector<int>& outFaceIndices) const;
+    glm::vec3 GetVertexNormal(int vertexIndex) const;
+    glm::vec3 GetFaceNormal(int faceIndex) const;
+
+    inline bool IsBoundaryVertex(int vertexIndex) const
     {
-        size_t hei = _vertexHalfEdges[vertexIndex];
-        return (_halfEdges[hei].faceIndex == INVALID_INDEX || _halfEdges[_halfEdges[hei].oppositeHalfEdge].faceIndex == INVALID_INDEX);
+        int hei = _vertexHalfEdges[vertexIndex];
+        return (_halfEdges[hei].faceIndex == -1 || _halfEdges[_halfEdges[hei].oppositeHalfEdge].faceIndex == -1);
     }
 
-    inline bool IsBoundaryEdge(size_t edgeIndex) const
+    inline bool IsBoundaryEdge(int edgeIndex) const
     {
-        size_t hei = _edgeHalfEdges[edgeIndex];
-        return (_halfEdges[hei].faceIndex == INVALID_INDEX || _halfEdges[_halfEdges[hei].oppositeHalfEdge].faceIndex == INVALID_INDEX);
+        int hei = _edgeHalfEdges[edgeIndex];
+        return (_halfEdges[hei].faceIndex == -1 || _halfEdges[_halfEdges[hei].oppositeHalfEdge].faceIndex == -1);
     }
 
-    void SplitEdge(size_t edgeIdx, float t);
+    inline int GetNumVertices()  const { return (int)_vertexHalfEdges.size(); }
+    inline int GetNumFaces()     const { return (int)_faceHalfEdges.size(); }
+    inline int GetNumEdges()     const { return (int)_edgeHalfEdges.size(); }
+    inline int GetNumHalfEdges() const { return (int)_halfEdges.size(); }
+
+    inline int GetHalfEdgeFromVertex(int vi) const { return _vertexHalfEdges[vi]; }
+    inline int GetHalfEdgeFromFace(int fi)   const { return _faceHalfEdges[fi]; }
+    inline int GetHalfEdgeFromEdge(int ei)   const { return _edgeHalfEdges[ei]; }
+
+    inline int ToVertex(int hei)   const { return _halfEdges[hei].toVertex; }
+    inline int FromVertex(int hei) const { return _halfEdges[_halfEdges[hei].oppositeHalfEdge].toVertex; }
+    inline int Face(int hei)       const { return _halfEdges[hei].faceIndex; }
+    inline int Edge(int hei)       const { return _halfEdges[hei].edgeIndex; }
+    inline int Opposite(int hei)   const { return _halfEdges[hei].oppositeHalfEdge; }
+    inline int Next(int hei)       const { return _halfEdges[hei].nextHlafEdge; }
+    inline int Prev(int hei)       const { return _halfEdges[_halfEdges[hei].nextHlafEdge].nextHlafEdge; }
+
+    inline glm::vec3 GetPoint(int vi) const { return _points[vi]; }
 
 private:
+    struct halfedge_t
+    {
+        int toVertex = -1;
+        int faceIndex = -1;
+        int edgeIndex = -1;
+        int oppositeHalfEdge = -1;
+        int nextHlafEdge = -1;
+    };
+
     void Clear();
-    glm::vec3 GetFaceNormalNotNormalized(size_t faceIndex) const;
+    glm::vec3 GetFaceNormalNotNormalized(int faceIndex) const;
 
     std::vector<glm::vec3> _points;
-    std::vector<size_t> _vertexHalfEdges;
-    std::vector<size_t> _faceHalfEdges;
-    std::vector<size_t> _edgeHalfEdges;
+    std::vector<int> _vertexHalfEdges;
+    std::vector<int> _faceHalfEdges;
+    std::vector<int> _edgeHalfEdges;
     std::vector<halfedge_t> _halfEdges;
-    std::map<std::pair<size_t, size_t>, size_t> _edgeToHalfEdge;
+    std::map<std::pair<int, int>, int> _edgeToHalfEdge;
 };
